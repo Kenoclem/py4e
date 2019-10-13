0d1ec8cf7296f4aad93a016200f389406c41efb9
Kenoclem <54127842+Kenoclem@users.noreply.github.com>
2019-10-14 00:56:43 +0800

    Add files via upload

--- exercise number 1.py
+++ exercise number 1.py
@@ -3,5 +3,5 @@ enter hours>? 50
 er= float(input("enter rate"))
 enter rate>? 4.5
 if eh > 50:
-    print("pay: ", (50*er)+(x-50)*4.5*er)
+    print("pay: ", (50*er)+(eh-50)*4.5*er)
 normalpay= float(eh*er)
